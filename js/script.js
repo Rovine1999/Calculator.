4611207f3eef3088b3e51d147018028813a28d83
Rovine1999 <rovinewanjala99@gmail.com>
2020-09-30 18:54:19 +0300

    added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,49 @@
-var add = function(number1, number2) {
-    return number1 + number2;
-  };
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = perseInt(prompt("Enter another number:"));
-  var results (add(number1, number2));
-  alert(results);
+// var add = function(number1, number2) {
+//     return number1 + number2;
+//   };
+//   var number1 = parseInt(prompt("Enter a number:"));
+//   var number2 = parseInt(prompt("Enter another number:"));
+//   var results = (number1, number2);
+//   alert(results);
+
+
+//    var subtract = function(number1, number2) {
+//         return number1 - number2;
+//       };
+//        var number1 = parseInt(prompt("Enter a number:"));
+//        var number2 = parseInt(prompt("Enter another number:"));
+//        var result = subtract(number1, number2);
+//        alert(result);
+
+
+    //    var divide = function(number1, number2) {
+    //     return number1 / number2;
+    //   };
+    //    var number1 = parseInt(prompt("Enter a number:"));
+    //    var number2 = parseInt(prompt("Enter another number:"));
+    //    var result = divide(number1, number2);
+    //    alert(result);
+
+
+    //    var divide = function(weight, height) {
+    //     return weight / height;
+    //   };
+    //    var weight = parseInt(prompt("Enter weight:"));
+    //    var height = parseInt(prompt("Enter height:"));
+    //    var result = convert(weight, height);
+    //    alert(result)
+
+    // var  add = function(celcius) {
+    //     return ((celcius *2)+30);
+    // }
+    // var celcius = parseInt(prompt("Enter degree"));
+    // var result = add(celcius);
+    // alert(result)
+
+
+    var  multiply = function(gallons) {
+        return ((gallons * 0.264));
+    }
+    var gallons = parseInt(prompt("Enter number of gallons"));
+    var result = multiply(gallons);
+    alert(result)
